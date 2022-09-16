637e448b8645c753e79e761e17e6167e4b7c89ca
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-09-16 18:48:33 +0300

    add routes, controllers, middleware

--- server/index.js
+++ server/index.js
@@ -4,16 +4,18 @@ const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
+const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandling')
 
 const PORT = process.env.port || 5000
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use('/api', router)
 
-app.get('/', (req, res) => {
-  res.status(200).json({message: 'WORKING'})
-})
+// Error handling, last Middleware
+app.use(errorHandler)
 
 const start = async () => {
   try {
