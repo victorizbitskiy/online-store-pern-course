eac332ff1c2a3a13abee0725e4790c7ac14a86b1
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-09-16 17:52:05 +0300

    add cors

--- server/index.js
+++ server/index.js
@@ -3,9 +3,17 @@ require('dotenv').config({path: `${__dirname}/.env`})
 const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
+const cors = require('cors')
+
 const PORT = process.env.port || 5000
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.status(200).json({message: 'WORKING'})
+})
 
 const start = async () => {
   try {
