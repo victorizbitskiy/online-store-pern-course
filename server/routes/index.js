7a8c6ce08bd32ac35e149e40c23fc2006236d711
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-09-17 08:34:53 +0300

    impl type controller

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,8 +6,8 @@ const brandRouter = require('./brandRouter')
 const deviceRouter = require('./deviceRouter')
 
 router.use('/user', userRouter)
-// router.use('/type', typeRouter)
-// router.use('/brand', brandRouter)
-// router.use('/device', deviceRouter)
+router.use('/type', typeRouter)
+router.use('/brand', brandRouter)
+router.use('/device', deviceRouter)
 
 module.exports = router
