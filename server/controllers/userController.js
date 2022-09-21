4f675857ec2c1be84a2cea5531d481439cae6a6f
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-09-21 19:13:18 +0300

    add sign in with jwt token

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,6 +3,14 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const { User, Basket } = require('../models/models')
 
+const generateJwt = (id, email, role) => {
+  return jwt.sign(
+    { id: id, email, role },
+    process.env.SECRET_KEY,
+    { expiresIn: '24h' }
+  )
+}
+
 class UserController {
   async registration(req, res, next) {
     const { email, password, role } = req.body
@@ -16,24 +24,26 @@ class UserController {
     const hasPassword = await bcrypt.hash(password, 5)
     const user = await User.create({ email, role, password: hasPassword })
     const basket = await Basket.create({ userId: user.id })
-    const token = jwt.sign(
-      { id: user.id, email, role },
-      process.env.SECRET_KEY,
-      { expiresIn: '24h' }
-    )
+    const token = generateJwt(user.id, user.email, user.role)
     return res.json({ token })
   }
 
-  async login(req, res) {
-
+  async login(req, res, next) {
+    const { email, password } = req.body
+    const user = await User.findOne({ where: { email } })
+    if (!user) {
+      return next(ApiError.internal('User is not found'))
+    }
+    let comparePassword = bcrypt.compareSync(password, user.password)
+    if (!comparePassword) {
+      return next(ApiError.internal('Invalid password'))
+    }
+    const token = generateJwt(user.id, user.email, user.role)
+    return res.json({ token })
   }
 
   async check(req, res, next) {
-    const { id } = req.query
-    if (!id) {
-      return next(ApiError.badRequest('ID must be set'))
-    }
-    res.json(id)
+
   }
 }
 
