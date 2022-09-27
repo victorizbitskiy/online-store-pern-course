dc5ecea1d88af117be37e31d60393a5d2c323e95
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-09-27 19:00:38 +0300

    create store

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import { Routes, Route} from 'react-router-dom'
+import React, { useContext } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Shop from '../pages/Shop';
 import { authRoutes, publicRoutes } from '../routes';
+import { Context } from '../index'
 
 const AppRouter = () => {
-  const isAuth = false
+  const { user } = useContext(Context)
+  console.log(user);
   return (
     <Routes>
-      {isAuth && authRoutes.map(({ path, element }) =>
+      {/* <Route path="/" element={<Shop />} /> */}
+      {user.isAuth && authRoutes.map(({ path, element }) =>
         <Route key={path} path={path} element={element} exact />
       )}
       {publicRoutes.map(({ path, element }) =>
         <Route key={path} path={path} element={element} exact />
       )}
+      {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
     </Routes>
   );
 };
