ae85a7117a20689b38d644d425a8ba1ac7c54421
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-10-07 14:33:13 +0300

    chng admin page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { Context } from "../index";
-import { SHOP_ROUTE } from "../utils/consts";
+import { ADMIN_ROUTE, SHOP_ROUTE } from "../utils/consts";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -9,7 +9,8 @@ import Button from "react-bootstrap/Button";
 import {observer} from "mobx-react-lite"
 
 const NavBar = observer(() => {
-  const { user } = useContext(Context);
+  const { user } = useContext(Context)
+  let navigate = useNavigate()
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
@@ -18,8 +19,13 @@ const NavBar = observer(() => {
         </NavLink>
         {user.isAuth ? (
           <Nav className="ml-auto" style={{ color: "white" }}>
-            <Button variant={"outline-light"}>Админ панель</Button>
-            <Button variant={"outline-light"} className="mx-4">Войти</Button>
+            <Button 
+            variant={"outline-light"}
+            onClick={() => navigate(ADMIN_ROUTE)}
+            >
+            Админ панель
+            </Button>
+            <Button variant={"outline-light"} className="mx-4">Выйти</Button>
           </Nav>
         ) : (
           <Nav className="ml-auto" style={{ color: "white" }}>
