1385e4e2d0796cd06e4092cd4fcedd3b68c340a9
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-10-13 19:12:01 +0300

    impl logout func

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,16 +1,22 @@
 import React, { useContext } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { Context } from "../index";
-import { ADMIN_ROUTE, BASKET_ROUTE, SHOP_ROUTE } from "../utils/consts";
+import { ADMIN_ROUTE, BASKET_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from "../utils/consts";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
-import {observer} from "mobx-react-lite"
+import { observer } from "mobx-react-lite"
 
 const NavBar = observer(() => {
   const { user } = useContext(Context)
   const navigate = useNavigate()
+
+  const logOut = () => {
+    user.setUser({})
+    user.setIsAuth(false)
+  }
+
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
@@ -19,17 +25,28 @@ const NavBar = observer(() => {
         </NavLink>
         {user.isAuth ? (
           <Nav className="ml-auto" style={{ color: "white" }}>
-            <Button 
-            variant={"outline-light"}
-            onClick={() => navigate(ADMIN_ROUTE)}
+            <Button
+              variant={"outline-light"}
+              onClick={() => navigate(ADMIN_ROUTE)}
             >
-            Админ панель
+              Админ панель
             </Button>
-            <Button variant={"outline-light"} className="mx-4">Выйти</Button>
+            <Button
+              variant={"outline-light"}
+              className="mx-4"
+              onClick={() => logOut()}
+            >
+              Выйти
+            </Button>
           </Nav>
         ) : (
           <Nav className="ml-auto" style={{ color: "white" }}>
-            <Button variant={"outline-light"} onClick={() => user.setIsAuth(true)}>Авторизация</Button>
+            <Button
+              variant={"outline-light"}
+              onClick={() => navigate(LOGIN_ROUTE)}
+            >
+              Авторизация
+            </Button>
           </Nav>
         )}
       </Container>
