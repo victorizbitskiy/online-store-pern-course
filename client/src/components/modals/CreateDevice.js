2602e8d3c1653fd46d188438a635b0fb976c6747
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-10-17 10:45:05 +0300

    impl device dropdown

--- client/src/components/modals/CreateDevice.js
+++ client/src/components/modals/CreateDevice.js
@@ -1,11 +1,20 @@
-import React, { useContext, useState } from "react";
+import { observer } from "mobx-react-lite";
+import React, { useContext, useEffect, useState } from "react";
 import { Button, Col, Dropdown, Form, Modal, Row } from "react-bootstrap";
-import DropdownMenu from "react-bootstrap/esm/DropdownMenu";
+import { fetchBrands, fetchTypes } from "../../http/deviceAPI";
 import { Context } from "../../index";
 
-const CreateDevice = ({ show, onHide }) => {
+const CreateDevice = observer(({ show, onHide }) => {
   const { device } = useContext(Context)
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState(0)
+  const [file, setFile] = useState(null)
   const [info, setInfo] = useState([])
+
+  useEffect(() => {
+    fetchTypes().then(data => device.setTypes(data))
+    fetchBrands().then(data => device.setBrands(data))
+  }, [])
 
   const addInfo = () => {
     setInfo([...info, { title: "", description: "", number: Date.now() }]);
@@ -13,6 +22,10 @@ const CreateDevice = ({ show, onHide }) => {
 
   const removeInfo = (number) => {
     setInfo(info.filter(i => i.number !== number));
+  }
+
+  const selectFile = e => {
+    setFile(e.target.files[0])
   }
 
   return (
@@ -24,31 +37,49 @@ const CreateDevice = ({ show, onHide }) => {
       </Modal.Header>
       <Modal.Body>
         <Dropdown className="mt-2 mb-2">
-          <Dropdown.Toggle>Выберите тип</Dropdown.Toggle>
-          <DropdownMenu>
+          <Dropdown.Toggle>{device.selectedType.name || "Выберите тип"}</Dropdown.Toggle>
+          <Dropdown.Menu>
             {device.types.map((type) => (
-              <Dropdown.Item key={type.id}>{type.name}</Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => device.setSelectedType(type)}
+                key={type.id}
+              >
+                {type.name}
+              </Dropdown.Item>
             ))}
-          </DropdownMenu>
+          </Dropdown.Menu>
         </Dropdown>
         <Dropdown className="mt-2 mb-2">
-          <Dropdown.Toggle>Выберите бренд</Dropdown.Toggle>
-          <DropdownMenu>
+          <Dropdown.Toggle>{device.selectedBrand.name || "Выберите тип"}</Dropdown.Toggle>
+          <Dropdown.Menu>
             {device.brands.map((brand) => (
-              <Dropdown.Item key={brand.id}>{brand.name}</Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => device.setSelectedBrand(brand)}
+                key={brand.id}
+              >
+                {brand.name}
+              </Dropdown.Item>
             ))}
-          </DropdownMenu>
+          </Dropdown.Menu>
         </Dropdown>
         <Form.Control
           className="mt-3"
           placeholder="Введите название устройства"
+          value={name}
+          onChange={e => setName(e.target.value)}
         />
         <Form.Control
           className="mt-3"
           placeholder="Введите стоимость устройства"
           type="number"
+          value={price}
+          onChange={e => setPrice(Number(e.target.value))}
         />
-        <Form.Control className="mt-3" type="file" />
+        <Form.Control
+          className="mt-3"
+          type="file"
+          onChange={selectFile}
+        />
         <hr />
         <Button variant={"outline-dark"} onClick={addInfo}>
           Добавить новое свойство
@@ -62,11 +93,11 @@ const CreateDevice = ({ show, onHide }) => {
               <Form.Control placeholder="Введите описание характеристики" />
             </Col>
             <Col md={4}>
-              <Button 
-              variant="outline-danger"
-              onClick={() => removeInfo(i.number)}
+              <Button
+                variant="outline-danger"
+                onClick={() => removeInfo(i.number)}
               >
-              Удалить
+                Удалить
               </Button>
             </Col>
           </Row>
@@ -81,7 +112,7 @@ const CreateDevice = ({ show, onHide }) => {
         </Button>
       </Modal.Footer>
     </Modal>
-  );
-};
+  )
+})
 
 export default CreateDevice;
