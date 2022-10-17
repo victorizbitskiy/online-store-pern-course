120f50911ea9592898541745ed8a68ce5cf7373d
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-10-17 10:45:05 +0300

    impl createbrand

--- client/src/components/modals/CreateBrand.js
+++ client/src/components/modals/CreateBrand.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form, Modal } from 'react-bootstrap';
+import { createBrand } from '../../http/deviceAPI';
 
-const CreateBrand = ({show, onHide}) => {
+const CreateBrand = ({ show, onHide }) => {
+  const [value, setValue] = useState('')
+
+  const addBrand = () => {
+    createBrand({ name: value }).then(data => {
+      setValue('')
+      onHide()
+    })
+  }
+
   return (
     <Modal show={show} onHide={onHide} centered>
       <Modal.Header closeButton>
@@ -11,14 +21,18 @@ const CreateBrand = ({show, onHide}) => {
       </Modal.Header>
       <Modal.Body>
         <Form>
-          <Form.Control placeholder={"Введите название типа..."} />
+          <Form.Control
+            placeholder={"Введите название типа..."}
+            value={value}
+            onChange={e => setValue(e.target.value)}
+          />
         </Form>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="outline-danger" onClick={onHide}>
           Закрыть
         </Button>
-        <Button variant="outline-success" onClick={onHide}>
+        <Button variant="outline-success" onClick={addBrand}>
           Добавить
         </Button>
       </Modal.Footer>
